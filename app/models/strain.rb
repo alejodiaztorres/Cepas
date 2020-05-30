d5305a677f02da37d3eca92c61313960d2f9ffd4
Alejandro <alejodiaz.123@gmail.com>
2020-05-30 18:16:43 -0400

    Validando que al crear un vino una cepa, estos no se encuentren vacíos

--- app/models/strain.rb
+++ app/models/strain.rb
@@ -1,4 +1,6 @@
 class Strain < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: true
     has_and_belongs_to_many :wines
     has_many :scores
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,5 @@
 class Wine < ApplicationRecord
+    validates :name, presence: true
     has_and_belongs_to_many :strains
     has_many :scores
     has_and_belongs_to_many :oenologists
